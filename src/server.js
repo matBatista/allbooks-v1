152271c4af01df6f975acfbe311fa91986803c81
Matheus Batista <matheus.batistacs@gmail.com>
2024-09-04 13:20:28 +1000

    Alteracao testes

--- src/server.js
+++ src/server.js
@@ -18,6 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
-  console.log("Teste Add");
+  console.log("Acessar essa API em http://localhost:8000")
 })
